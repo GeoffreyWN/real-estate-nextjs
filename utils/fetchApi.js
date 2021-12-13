c0cf6c8139554b601bab0b69ada154f0b87a9da8
weshsnr <ndungugeoffrey11@gmail.com>
2021-12-13 22:53:54 +0300

    updated readme

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,34 +1,14 @@
-// import axios from 'axios'
-
-// export const baseUrl = process.env.NEXT_PUBLIC_BASEURL
-
-// export const fetchApi = async (url) => {
-//     const { data } = await axios.get(url, {
-//         headers: {
-//             'x-rapidapi-host': 'bayut.p.rapidapi.com',
-//             'x-rapidapi-key': '30f82e3c0bmsh9633a566c885d62p1edaecjsn78093054b47e',
-//         }
-//     })
-
-//     return data
-// }
-
 import axios from "axios";
 
-export const baseUrl = process.env.BASEURL //'https://bayut.p.rapidapi.com';
+export const baseUrl = process.env.BASEURL
 
 
 export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
         headers: {
-            'x-rapidapi-host': process.env.X_RAPIDAPI_HOST,//'bayut.p.rapidapi.com',
-            'x-rapidapi-key': process.env.X_RAPIDAPI_KEY //'30f82e3c0bmsh9633a566c885d62p1edaecjsn78093054b47e'
+            'x-rapidapi-host': process.env.X_RAPIDAPI_HOST,
+            'x-rapidapi-key': process.env.X_RAPIDAPI_KEY
         }
-
-        // headers: {
-        //     'x-rapidapi-host': 'bayut.p.rapidapi.com',
-        //     'x-rapidapi-key': '30f82e3c0bmsh9633a566c885d62p1edaecjsn78093054b47e',
-        // }
     });
 
     return data;
