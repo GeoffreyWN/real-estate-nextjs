e5b069dfb8a283787d9c2543b93c29deaf4838f7
weshsnr <ndungugeoffrey11@gmail.com>
2021-12-13 13:50:57 +0300

    updated landing page

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+import NProgress from 'nprogress'
+
+import Router from 'next/router'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
